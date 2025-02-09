216e55200d0c0a2f97a39ba2f58fb8e2cad77c63
Armando Picón <picondev@gmail.com>
2025-02-09 17:45:00 -0300

    add json data and custom error

---
+++ lib/shared/data/menu_data.dart
@@ -0,0 +1,52 @@
+final menuData = [
+  {
+    "name": "Risotto alla Milanese",
+    "description":
+        "A creamy Italian rice dish cooked with saffron, butter, and Parmesan cheese."
+  },
+  {
+    "name": "Margherita Pizza",
+    "description":
+        "A classic Italian pizza topped with tomato sauce, fresh mozzarella, and basil leaves."
+  },
+  {
+    "name": "Chicken Tikka Masala",
+    "description":
+        "Tender chicken chunks marinated in yogurt and spices, cooked in a rich tomato-based curry sauce."
+  },
+  {
+    "name": "Dal Makhani",
+    "description":
+        "A flavorful Indian dish made with slow-cooked black lentils and kidney beans in a creamy tomato sauce."
+  },
+  {
+    "name": "Coq au Vin",
+    "description":
+        "A traditional French dish of chicken slow-cooked in red wine with mushrooms, onions, and bacon."
+  },
+  {
+    "name": "Ratatouille",
+    "description":
+        "A classic French vegetable stew made with eggplant, zucchini, bell peppers, and tomatoes."
+  },
+  {
+    "name": "Lomo Saltado",
+    "description":
+        "A popular Peruvian stir-fry dish with marinated beef, onions, tomatoes, and French fries, served with rice."
+  },
+  {
+    "name": "Ceviche",
+    "description":
+        "A Peruvian dish of fresh raw fish cured in lime juice, mixed with onions, chili peppers, and cilantro."
+  },
+  {
+    "name": "Salteñas",
+    "description":
+        "Bolivian savory pastries filled with a juicy mixture of meat, potatoes, peas, and a slightly sweet and spicy sauce."
+  },
+  {
+    "name": "Sajta de Pollo",
+    "description":
+        "A traditional Bolivian chicken stew cooked with a spicy peanut-based sauce, served with rice and potatoes."
+  }
+];
